3da2b5a1a3322e2fb2f1f54d8482caefa4ccc4d8
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2025-07-27 02:57:53 +0200

    iterate over directories in /

--- src/init.zig
+++ src/init.zig
@@ -20,11 +20,30 @@ pub fn main() noreturn {
         "/dev/console",
         .{ .mode = .read_write },
     ) catch |e| {
-        panic("Could not open console.\n{any}\n", .{e});
+        panic("Could not open /dev/console.\n{any}\n", .{e});
     };
 
+    const root_dir = std.fs.openDirAbsolute("/", .{ .iterate = true }) catch |e|
+        panic("Could not open root directory.\n{any}\n", .{e});
+
     while (true) {
-        console.writeAll(io, "hello, world!\n") catch {};
+        var root_it = root_dir.iterate();
+        while (root_it.next() catch |e|
+            panic("Error while walking root directory.\n{any}\n", .{e})) |subdir|
+        {
+            console.writeAll(io, "\n") catch {};
+            console.writeAll(io, subdir.name) catch {};
+            var dir = root_dir.openDir(subdir.name, .{ .iterate = true }) catch continue;
+            defer dir.close();
+            console.writeAll(io, ":") catch {};
+            var it = dir.iterate();
+            while (it.next() catch |e|
+                panic("Error while walking /{s} directory.\n{any}\n", .{ subdir.name, e })) |item|
+            {
+                console.writeAll(io, "\n  ") catch {};
+                console.writeAll(io, item.name) catch {};
+            }
+        }
         std.Thread.sleep(1 * std.time.ns_per_s);
     }
 }
