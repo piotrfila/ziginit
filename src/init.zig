bb79d4c3b2b0cd44ae32f1cb214fc0879469091f
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2025-07-27 03:49:21 +0200

    go back to zig 0.14.1

--- src/init.zig
+++ src/init.zig
@@ -7,18 +7,9 @@ pub fn main() noreturn {
     if (std.os.linux.getpid() != 1)
         panic("This program is meant to be run as init.\n", .{});
 
-    var gpa_state: std.heap.DebugAllocator(.{}) = .init;
-    defer _ = gpa_state.deinit();
-    const gpa = gpa_state.allocator();
-
-    var pool: Io.ThreadPool = .init(gpa);
-    defer pool.deinit();
-    const io = pool.io();
-
-    const console = Io.Dir.cwd().openFile(
-        io,
+    const console = std.fs.openFileAbsolute(
         "/dev/console",
-        .{ .mode = .read_write },
+        .{ .mode = .write_only },
     ) catch |e| {
         panic("Could not open /dev/console.\n{any}\n", .{e});
     };
@@ -31,17 +22,17 @@ pub fn main() noreturn {
         while (root_it.next() catch |e|
             panic("Error while walking root directory.\n{any}\n", .{e})) |subdir|
         {
-            console.writeAll(io, "\n") catch {};
-            console.writeAll(io, subdir.name) catch {};
+            console.writeAll("\n") catch {};
+            console.writeAll(subdir.name) catch {};
             var dir = root_dir.openDir(subdir.name, .{ .iterate = true }) catch continue;
             defer dir.close();
-            console.writeAll(io, ":") catch {};
+            console.writeAll(":") catch {};
             var it = dir.iterate();
             while (it.next() catch |e|
                 panic("Error while walking /{s} directory.\n{any}\n", .{ subdir.name, e })) |item|
             {
-                console.writeAll(io, "\n  ") catch {};
-                console.writeAll(io, item.name) catch {};
+                console.writeAll("\n  ") catch {};
+                console.writeAll(item.name) catch {};
             }
         }
         std.Thread.sleep(1 * std.time.ns_per_s);
